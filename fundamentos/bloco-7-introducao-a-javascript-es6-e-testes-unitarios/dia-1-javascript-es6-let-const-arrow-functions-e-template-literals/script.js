fce8f985aa188949e35f6f844cd733a97cdc0840
Ques-Mendes <quesmendes@gmail.com>
2021-12-03 16:05:50 -0300

    Exercicio 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -10,7 +10,7 @@
         console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
       } */
 
-      const testingScope = escopo => {
+   /*    const testingScope = escopo => {
           if (escopo === true) { 
             let ifScope = 'Não devo ser  utilizada fora do meu escopo (if)';
             ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
@@ -20,4 +20,20 @@
               console.log(elseScope);
           }
       }
-      testingScope(true);
+      testingScope(true); */
+
+      const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+      const sortOddsAndEvens = () => {
+          oddsAndEvens[0] = 2;
+          oddsAndEvens[1] = 3;
+          oddsAndEvens[2] = 4;
+          oddsAndEvens[3] = 7;
+          oddsAndEvens[4] = 10;
+          oddsAndEvens[5] = 13;
+          return oddsAndEvens;
+      }
+      const sortedNumbers = sortOddsAndEvens();
+      console.log(`Os números ${sortedNumbers} se encontram ordenados de forma crescente!`);
+
+      
