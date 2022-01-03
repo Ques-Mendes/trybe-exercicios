e2e4689bbc72477e0fc183b69449c59714404aa7
Ques-Mendes <quesmendes@gmail.com>
2022-01-03 18:23:03 -0300

    exercise6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise6.js
@@ -69,5 +69,8 @@ const expectedResult = [
 ];
 
 function oldBooks() {
-  // escreva seu código aqui
+  return books.filter((book) => (
+    book.releaseYear < 1962
+  )).map((book) =>book.name);
 }
+console.log(oldBooks(books));
