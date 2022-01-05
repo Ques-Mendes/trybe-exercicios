f0e863fe03fdc94ab8492d7bcd7aea939867c082
Ques-Mendes <quesmendes@gmail.com>
2022-01-05 19:02:19 -0300

    exercise 8 and 9

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
@@ -1,4 +1,5 @@
 // escreva greet abaixo
+const greet = (name, message = 'Hi') => `${message} ${name}`;
 
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
