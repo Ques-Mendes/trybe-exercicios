1cbb17f4d8c0bf06a0413d3c7ec7cd4901880dc1
Ques-Mendes <quesmendes@gmail.com>
2022-01-05 14:41:02 -0300

    exercise2 working on it

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise2.js
@@ -65,5 +65,7 @@ const books = [
 const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
 function reduceNames() {
-  // escreva seu código aqui
+  const authorNames = books.reduce((acc, curr) => {
+    return `${books.author.name},`;
+  }, '');
 }
