03046ee0b8075cf85b2f8b7e0ebc6014edb42716
Ques-Mendes <quesmendes@gmail.com>
2022-02-17 15:07:50 -0300

    Faz  implementaçao com um param

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.test.js
@@ -1,10 +1,23 @@
 const service = require("./service");
+describe("Testes de implementações", () => {
+  test("Com mock função recebe 3 parâmetros e retorna a multiplicação dos mesmos", () =>{
+    service.randomNumber = jest.fn().mockImplementationOnce((a,b,c) => a * b * c);
 
-test("Verfica se a função foi chamada, qual seu retorno e quantas vezes foi chamada e com dois parametros", () =>{
-    service.randomNumber = jest.fn().mockImplementationOnce((a,b) => a/b);
-
-    expect(service.randomNumber(20,2)).toBe(10);
+    expect(service.randomNumber(2,4,6)).toBe(48);
     expect(service.randomNumber).toHaveBeenCalled();
     expect(service.randomNumber).toHaveBeenCalledTimes(1);
-    expect(service.randomNumber).toHaveBeenCalledWith(20,2);
+    expect(service.randomNumber).toHaveBeenCalledWith(2,4,6);
+  });
+
+  test("Com mock função recebe 1 parâmetro e retorna o dobro", () => {
+    service.randomNumber.mockReset();
+    expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+    service.randomNumber.mockImplementation(a => a * 2);
+
+    expect(service.randomNumber(2)).toBe(4);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(2);
+  });
 });
