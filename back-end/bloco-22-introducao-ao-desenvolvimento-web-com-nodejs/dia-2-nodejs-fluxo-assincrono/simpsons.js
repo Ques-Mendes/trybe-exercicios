4520a99157f2096a3935ec057693669ea60220ca
Ques-Mendes <quesmendes@gmail.com>
2022-05-11 16:41:43 -0300

    Get simpson by id

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 
-async function readAll() {
+/* async function readAll() {
   const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
   const simpsons = JSON.parse(fileContent);
   const contentValues = simpsons.map(({ id, name }) => `${id} - ${name}`);
@@ -8,7 +8,26 @@ async function readAll() {
 }
 
 function main() {
-  readAll();
+  readAll();  
+}
+main(); */
+
+async function getSimpsonById(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
+  const simpsons = JSON.parse(fileContent);
+  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+  if (!chosenSimpson) {
+    throw new Error('id não encontrado');
+  }
+  return chosenSimpson;
 }
 
+/* function main() {
+  getSimpsonById();
+} */
+
+async function main() {
+  const simpson = await getSimpsonById(4);
+  console.log(simpson);
+}
 main();
