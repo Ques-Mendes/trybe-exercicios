d8bbd1035db40a2b722064b4fc514bee39f9a933
Ques-Mendes <quesmendes@gmail.com>
2022-05-11 18:22:43 -0300

    Create simpsons family

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
@@ -1,5 +1,7 @@
 const fs = require('fs').promises;
+
 /* 
+Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson.
 async function readAll() {
   const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
   const simpsons = JSON.parse(fileContent);
@@ -12,7 +14,9 @@ function main() {
 }
 main();  */
 
-/* async function getSimpsonById(id) {
+/*
+Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+ async function getSimpsonById(id) {
   const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
   const simpsons = JSON.parse(fileContent);
   const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
@@ -29,6 +33,7 @@ async function main() {
 
 main(); */
 
+/* Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
 async function filterSimpsons() {
   const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
   const simpsons = JSON.parse(fileContent);
@@ -38,6 +43,20 @@ async function filterSimpsons() {
 
 async function main() {
   const simpson = await filterSimpsons();
+  console.log(simpson);
+}
+main(); */
+
+async function simpsonFamily() {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8')
+  const simpsons = JSON.parse(fileContent);
+  const familyIds = [1, 2, 3, 4];
+  const simpsonFamily = simpsons.filter((simpson) => familyIds.includes(Number(simpson.id)));
+  await fs.writeFile('./simpsonsFamily.json', JSON.stringify(simpsonFamily));
+}
+
+async function main() {
+  const simpson = await simpsonFamily();
   console.log(simpson);
 }
 main();
