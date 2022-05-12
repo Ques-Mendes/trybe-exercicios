dedc53f2bc4cf59fd341dfa927e100510311859e
Ques-Mendes <quesmendes@gmail.com>
2022-05-12 20:27:51 -0300

    Create POST /greetings

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -7,9 +7,20 @@ app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => res.json({ "message": "pong" }));
 
-app.post('/ping', (_req, res) => {
+app.post('/hello', (req, res) => {
   const { name } = req.body;
+  console.log("test", name); // name is undefined ?
   return res.status(200).json({ "message": `Hello, ${name}!` });
-})
+}) 
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if (parseInt(age, 10) <= 17) {
+    return res.status(400).json({ message: `Unauthorized` });
+  }  
+  return res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+
 
 app.listen(3000, () => console.log('ouvindo na porta 3000!'));
