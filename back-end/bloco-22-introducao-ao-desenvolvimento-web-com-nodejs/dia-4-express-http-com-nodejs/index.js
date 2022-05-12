3fdfd04c85f7d79ec8ebf6f63e04df09220c4a0a
Ques-Mendes <quesmendes@gmail.com>
2022-05-12 20:09:36 -0300

    Create post /hello

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(bodyParser.json());
 
-app.get('/ping', function(req, res) {
-  res.json({ message: 'pong' })
-});
+app.get('/ping', (req, res) => res.json({ "message": "pong" }));
 
+app.post('/ping', (_req, res) => {
+  const { name } = req.body;
+  return res.status(200).json({ "message": `Hello, ${name}!` });
+})
 
-// const PORT = process.env(3000);
-app.listen(3000, () => console.log('listening on port 3000!'));
+app.listen(3000, () => console.log('ouvindo na porta 3000!'));
