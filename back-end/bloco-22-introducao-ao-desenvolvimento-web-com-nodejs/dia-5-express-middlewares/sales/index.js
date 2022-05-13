de1888ef2b3890eb6f7c6d4fe7fcec1ff63faac9
Ques-Mendes <quesmendes@gmail.com>
2022-05-13 19:41:59 -0300

    Creat mid err

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/sales/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/sales/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { errorHandler } = require("./middlewares");
 const app = express();
 const port = 3000;
 app.use(express.json());
 
 app.use("/sales", require('./routes/sales'));
 
+app.use(errorHandler);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/sales/middlewares/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/sales/middlewares/index.js
@@ -2,10 +2,12 @@ const validateProductName = require("./validateProductName");
 const validateInfo = require("./validateInfo");
 const validateSaleDate = require("./validateSaleDate");
 const validateWarranty = require("./validateWarranty");
+const errorHandler = require("./errorHandler");
 
 module.exports = {
   validateProductName,
   validateInfo,
   validateSaleDate,
   validateWarranty,
+  errorHandler,
 };
