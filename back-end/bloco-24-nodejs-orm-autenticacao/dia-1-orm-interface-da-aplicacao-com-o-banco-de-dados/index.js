89f18df1fc9a0a41ec6d1becf0ea2d1ed6d2180f
Ques-Mendes <quesmendes@gmail.com>
2022-06-06 19:24:08 -0300

    add route to index

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const booksController = require('./controllers/booksController');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.get('/books', booksController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
