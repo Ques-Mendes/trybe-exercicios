ab0f84984cd0d84fe1164eed3a6cf65ad9fcd037
Ques-Mendes <quesmendes@gmail.com>
2022-07-04 18:19:11 -0300

    exercise2 create an enum for months

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/dist/months.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var months;
+(function (months) {
+    months["January"] = "January";
+    months["February"] = "February";
+    months["March"] = "March";
+    months["April"] = "April";
+    months["May"] = "May";
+    months["June"] = "June";
+    months["July"] = "July";
+    months["August"] = "August";
+    months["September"] = "September";
+    months["October"] = "October";
+    months["November"] = "November";
+    months["December"] = "December";
+})(months || (months = {}));
+exports.default = months;
