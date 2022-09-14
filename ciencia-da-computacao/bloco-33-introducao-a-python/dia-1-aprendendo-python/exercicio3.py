8cd8b2ec1dc4f6f9ab56192b509c8684fc1cf3fe
quesia <quesmendes@gmail.com>
2022-09-14 13:06:20 -0300

    fix: linter

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercicio3.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercicio3.py
@@ -1,6 +1,7 @@
 # Faça um programa que, dado um valor n qualquer, tal que n > 1,
 # imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
-def square_asterisk(n): 
+
+def square_asterisk(n):
     for row in range(n):
-        print(n * '*')
+        print(n * "*")
