dfd154e55a9a411ec822f7862e8bb90109676d79
quesia <quesmendes@gmail.com>
2022-09-15 18:06:54 -0300

    in process

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio4.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio4.py
@@ -1,13 +1,18 @@
+# calcule a porcentagem de livros em cada categoria, em relação ao número
+# total de livros. O resultado deve ser escrito em um arquivo no formato CSV
+
 import json
-import csv
 
 
 def get_books(file):
     return json.load(file)
+
 
 def total_books_by_categories(books):
     categories = {}
     for book in books:
         for category in book["categories"]:
             if not categories.get(category):
-                
+                categories[category] = 0
+            categories[category] += 1
+        return categories
